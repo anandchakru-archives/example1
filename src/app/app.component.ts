16254bb34fb74b7974ada41d2f29bb524d5357e1
Anand <anandchakru@gmail.com>
2019-06-28 23:02:02 -0700

    sample invite created.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'nivite-root',
@@ -6,11 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'ng8';
+  @ViewChild('container', { static: false }) container: ElementRef;
+  loaded: boolean;
   invite;
+  carousalReady: boolean;
+
   ngOnInit() {
     document.addEventListener('niviteLoaded', (event) => {
       this.invite = ((event as CustomEvent).detail);
+      this.loaded = true;
     });
+  }
+  onCarouseliReady() {
+    this.carousalReady = true;
+  }
+  action(): string {
+    if (this.invite.customerInvite) {
+      switch (this.invite.customerInvite.rsvp) {
+        case 'Y':
+        case 'N':
+        case 'M':
+          return 'You have already responded ';
+        default:
+          return 'Respond to the invitation.';
+      }
+    } else {
+      return 'Respond to the invitation.';
+    }
+  }
+  public navigate() {
+    this.talkToNivite('niviteNavigate', {});
+  }
+  public addToCalendar() {
+    this.talkToNivite('niviteShowAddToCalendarModal', {});
+  }
+  public rsvp() {
+    this.talkToNivite('niviteShowRsvpModal', { changeEmail: false });
+  }
+  public changeEmailAddress() {
+    this.talkToNivite('niviteShowRsvpModal', { changeEmail: true });
+  }
+  private talkToNivite(eventId, data) {
+    const customEvent = document.createEvent('CustomEvent');
+    customEvent.initCustomEvent(eventId, true, false, data);
+    (this.container.nativeElement as HTMLDivElement).dispatchEvent(customEvent);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { HttpClient } from '@angular/common/http';
+import { LoadingComponent } from './util-components/loading/loading.component';
+import { CarouseliComponent } from './util-components/carouseli/carouseli.component';
+import { AtomSpinnerModule } from 'angular-epic-spinners';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouseliComponent,
+    LoadingComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AtomSpinnerModule,
+    MarkdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
