51941687d98834535b8e1df26508a3c3bbae33f1
Anand <anandchakru@gmail.com>
2019-06-28 21:54:39 -0700

    listen for inviteLoaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'nivite-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng8';
+  invite;
+  ngOnInit() {
+    document.addEventListener('niviteLoaded', (event) => {
+      this.invite = ((event as CustomEvent).detail);
+    });
+  }
 }
